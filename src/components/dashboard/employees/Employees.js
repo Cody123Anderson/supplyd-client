ef85d3430f12c05f514f3e2615cdcfb3508af2f3
Tanner Rigby <tanner.rigby@payclip.com>
2018-09-11 17:03:00 -0600

    reformat buttons

--- src/components/dashboard/employees/Employees.js
+++ src/components/dashboard/employees/Employees.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
 import MdAddCircle from 'react-icons/lib/md/add-circle';
 import MuiButton from '@material-ui/core/Button';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import './Employees.scss';
 import Button from '../../ui-components/Button';
 import {
@@ -11,7 +11,9 @@ import {
   DialogContent,
   DialogContentText,
   DialogTitle,
+  Slide,
 } from '@material-ui/core';
+import Close from '@material-ui/icons/Close';
 import TitleBar from '../TitleBar';
 import routes from '../../../constants/routes';
 import Table from '../../ui-components/Table';
@@ -20,7 +22,28 @@ import Footer from '../../Footer/Footer';
 import constants from '../../../constants';
 import { DOMAIN_URL } from '../../../constants/env';
 import Loader from '../../ui-components/Loader';
-import Slide from '@material-ui/core/Slide';
+
+const theme = createMuiTheme({
+  overrides: {
+    MuiButton: {
+      root: {
+        color: '#777',
+
+        '&:hover': {
+          backgroundColor: 'none',
+        },
+      },
+    },
+  },
+});
+
+const closebtnStyles = {
+  float: 'right',
+  marginRight: 20,
+  marginTop: -45,
+  fill: '#777',
+  cursor: 'pointer',
+};
 
 const columns = [
   {
@@ -54,7 +77,7 @@ class Employees extends Component {
     this.props.getEmployees(this.props.businessId).then(() => this.setState({ loading: false }));
   }
 
-  handleCancel = () => {
+  handleClose = () => {
     this.setState({ modalOpen: false, showUrl: false });
   };
 
@@ -90,17 +113,21 @@ class Employees extends Component {
     const { business } = this.props;
 
     const link = encodeURI(`${DOMAIN_URL}/swag/employee/${business.name}/${business.id}`);
-
     return (
       <Dialog
         TransitionComponent={Transition}
-        disableBackdropClick
-        disableEscapeKeyDown
+        fullWidth
         maxWidth="md"
+        PaperProps={{style: {margin: 15}}}
         aria-labelledby="add-new-employee-modal"
         open={this.state.modalOpen}
       >
-        <DialogTitle id="add-employee-dialog-title">Add Employee</DialogTitle>
+        <div>
+          <DialogTitle id="add-employee-dialog-title">
+            <div>Add Employee</div>
+          </DialogTitle>
+          <Close onClick={this.handleClose} style={closebtnStyles} />
+        </div>
         {this.state.showUrl ? (
           <div>
             <DialogContent style={{ textAlign: 'center' }}>
@@ -110,17 +137,19 @@ class Employees extends Component {
             </DialogContent>
 
             <DialogActions>
-              <MuiButton onClick={this.handleCancel} color="secondary">
-                Close
-              </MuiButton>
-              <MuiButton onClick={this.handleBack} style={{ color: '#43c59b' }}>
-                Go Back
-              </MuiButton>
+              <MuiThemeProvider theme={theme}>
+                <MuiButton onClick={this.handleBack}>
+                  Go Back
+                </MuiButton>
+              </MuiThemeProvider>
+              <Button onClick={this.handleClose} color="primary">
+                Done
+              </Button>
             </DialogActions>
           </div>
         ) : (
           <div>
-            <DialogContent>
+            <DialogContent style={{ textAlign: 'center', fontSize: 18 }}>
               <div>Which type of employee would you like to add?</div>
             </DialogContent>
             <DialogActions style={{ justifyContent: 'center', marginBottom: 30 }}>
@@ -131,40 +160,9 @@ class Employees extends Component {
                 Existing Employee
               </Button>
             </DialogActions>
-            <DialogActions>
-              <MuiButton onClick={this.handleCancel} color="secondary">
-                Close
-              </MuiButton>
-            </DialogActions>
           </div>
         )}
       </Dialog>
-    );
-  };
-
-  renderExistingLink = () => {
-    const { business } = this.props;
-
-    const link = encodeURI(`${DOMAIN_URL}/swag/employee/${business.name}/${business.id}`);
-
-    return (
-      <div>
-        <DialogContent>
-          Click the link below to copy it to your clipboard and send it to your existing employees.
-          Once the employee fills out the form, their swag will be on its way!
-        </DialogContent>
-        <DialogContent>
-          <div className="e-link">
-            {this.state.copiedExisting ? (
-              <div className="copied-text">URL copied to clipboard.</div>
-            ) : (
-              <CopyToClipboard text={link} onCopy={() => this.onCopyLink('copiedExisting')}>
-                {link}
-              </CopyToClipboard>
-            )}
-          </div>
-        </DialogContent>
-      </div>
     );
   };
 
